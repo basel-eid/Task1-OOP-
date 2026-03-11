92bfcb577517fcbaa74919324fe400d1f3f90964
Basel <bbakr3229@gmail.com>
2026-03-11 16:08:09 +0200

    Add Enum to select from it the payment type

---
+++ Enums/PaymentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Enums
+{
+    public enum PaymentType
+    {
+        Card,
+        Cash,
+        Installment
+    }
+}
