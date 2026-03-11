e8529d07ea6d316a59036adaebc7290a6a632dad
Basel <bbakr3229@gmail.com>
2026-03-11 16:10:11 +0200

    Added request and response so i don;t pass the varameters as values and send it as an object

---
+++ Models/PaymentContracts.cs
@@ -0,0 +1,16 @@
+﻿using PMS.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Models
+{
+    public class PaymentContracts
+    {
+        public record PaymentRequest(PaymentType Type, double Amount, int Months = 12);
+
+        public record PaymentResponse(bool IsSuccess, string Message, Payment? Data);
+    }
+}
